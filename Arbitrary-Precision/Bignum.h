8a70cc2a8d3fa34af7c7880536226ffde1b55c2c
lybin_win <1336258176@qq.com>
2023-02-15 20:19:39 +0800

    fixed Bignum code structure

--- Arbitrary-Precision/Bignum.h
+++ Arbitrary-Precision/Bignum.h
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <string>
 
 class Bignum
 {
@@ -9,12 +9,53 @@ private:
     std::vector<int> num;
 
 public:
-    Bignum(){}
-    ~Bignum(){}
+    Bignum() {}
+    ~Bignum() {}
 
-    Bignum operator= (const std::string t);
-    void operator= (const Bignum &t);
-    friend std::istream &operator>> (std::istream &in, Bignum &t);
-    friend std::ostream &operator<< (std::ostream &out, const Bignum &t);
+    Bignum operator=(const std::string t);
+    void operator=(const Bignum &t);
 
+    friend std::istream &operator>>(std::istream &in, Bignum &t);
+    friend std::ostream &operator<<(std::ostream &out, const Bignum &t);
+
+    Bignum operator+ (const Bignum &t);
+
+    Bignum operator- (const Bignum &t);
 };
+
+Bignum Bignum::operator=(const std::string t)
+{
+    this->num.clear();
+    for (int i = t.length() - 1; i >= 0; i--)
+    {
+        this->num.push_back(t[i] - 48);
+    }
+    return *this;
+}
+
+void Bignum::operator=(const Bignum &t)
+{
+    this->num = t.num;
+}
+
+std::istream &operator>>(std::istream &in, Bignum &t)
+{
+    std::string m;
+    in >> m;
+    t = m;
+    return in;
+}
+
+std::ostream &operator<<(std::ostream &out, const Bignum &t)
+{
+    for (int i = t.num.size() - 1; i >= 0; i--)
+    {
+        out << t.num[i];
+    }
+    return out;
+}
+
+Bignum Bignum::operator+ (const Bignum &t)
+{
+
+}
