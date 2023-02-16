51947a04bb351c94979c1a2c46f8a49b37e33df3
lybin_win <1336258176@qq.com>
2023-02-16 11:06:43 +0800

    perfect Bignum-addition

--- Arbitrary-Precision/Bignum.h
+++ Arbitrary-Precision/Bignum.h
@@ -18,7 +18,7 @@ public:
     friend std::istream &operator>>(std::istream &in, Bignum &t);
     friend std::ostream &operator<<(std::ostream &out, const Bignum &t);
 
-    Bignum operator+ (const Bignum &t);
+    Bignum operator+ (Bignum &t);
 
     Bignum operator- (const Bignum &t);
 };
@@ -26,8 +26,11 @@ public:
 Bignum Bignum::operator=(const std::string t)
 {
     this->num.clear();
+    
+    //倒装——从低位到高位压入vector
     for (int i = t.length() - 1; i >= 0; i--)
     {
+        //数字和与其对应的ASCII码转换——(num + 48 = ASCII) 
         this->num.push_back(t[i] - 48);
     }
     return *this;
@@ -48,6 +51,7 @@ std::istream &operator>>(std::istream &in, Bignum &t)
 
 std::ostream &operator<<(std::ostream &out, const Bignum &t)
 {
+    //注意逆序输出
     for (int i = t.num.size() - 1; i >= 0; i--)
     {
         out << t.num[i];
@@ -55,7 +59,49 @@ std::ostream &operator<<(std::ostream &out, const Bignum &t)
     return out;
 }
 
-Bignum Bignum::operator+ (const Bignum &t)
+Bignum Bignum::operator+ (Bignum &t)
 {
+    //取最大位数
+    int MAX = this->num.size() > t.num.size() ? this->num.size() : t.num.size();
 
+    //用0补位
+    if(MAX == this->num.size())
+    {
+        for(int i = 1; i <= MAX-t.num.size(); i++)
+        {
+            t.num.push_back(0);
+        }
+    }else
+    {
+        for(int i = 1; i <= MAX-this->num.size(); i++)
+        {
+            this->num.push_back(0);
+        }
+    }
+
+    //构造返回函数
+    Bignum a;
+    for(int i = 0; i < MAX; i++)
+    {
+        a.num.push_back(this->num[i] + t.num[i]);
+    }
+
+    //加法计算
+    for(int i = 0; i < MAX; i++)
+    {
+        if(a.num[i] >= 10)
+        {
+            //判断最高位是否需要进位
+            if(i == MAX-1)
+            {
+                a.num.push_back(a.num[i] / 10);
+                a.num[i] = a.num[i] % 10;
+            }else
+            {
+                a.num[i+1] += a.num[i] / 10;
+                a.num[i] = a.num[i] % 10;
+            }
+        }
+    }
+    return a;
 }
