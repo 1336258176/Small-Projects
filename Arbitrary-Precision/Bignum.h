37eb174bba83598b2e34379f6cddce46b7f196bc
lybin_win <1336258176@qq.com>
2023-02-17 22:15:47 +0800

    added Bignum-Multiply

--- Arbitrary-Precision/Bignum.h
+++ Arbitrary-Precision/Bignum.h
@@ -1,3 +1,6 @@
+#ifndef BIGNUM_H
+#define BIGNUM_H // define保护
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -6,31 +9,37 @@ class Bignum
 {
 
 private:
-    std::vector<int> num;
+    std::vector<int> num; // 储存数字
+    // bool flag;                              //判断是否为负
 
 public:
     Bignum() {}
     ~Bignum() {}
 
-    Bignum operator=(const std::string t);
-    void operator=(const Bignum &t);
+    inline Bignum operator=(const std::string t);
+    inline void operator=(const Bignum &t);
+    inline void operator=(const long long t);
 
     friend std::istream &operator>>(std::istream &in, Bignum &t);
-    friend std::ostream &operator<<(std::ostream &out, const Bignum &t);
+    friend std::ostream &operator<<(std::ostream &out, Bignum &t);
 
-    Bignum operator+ (Bignum &t);
+    inline Bignum operator+(Bignum &t);
+    inline Bignum operator+(const long long t);
 
-    Bignum operator- (const Bignum &t);
+    // inline Bignum operator- (Bignum &t);
+
+    inline Bignum operator*(const long long t);
+    inline Bignum operator*(const Bignum &t);
 };
 
 Bignum Bignum::operator=(const std::string t)
 {
     this->num.clear();
-    
-    //倒装——从低位到高位压入vector
+
+    // 倒装——从低位到高位压入vector
     for (int i = t.length() - 1; i >= 0; i--)
     {
-        //数字和与其对应的ASCII码转换——(num + 48 = ASCII) 
+        // 数字和与其对应的ASCII码转换——(num + 48 = ASCII)
         this->num.push_back(t[i] - 48);
     }
     return *this;
@@ -38,7 +47,13 @@ Bignum Bignum::operator=(const std::string t)
 
 void Bignum::operator=(const Bignum &t)
 {
+    this->num.clear();
     this->num = t.num;
+}
+
+void Bignum::operator=(const long long t)
+{
+    *this = std::to_string(t);
 }
 
 std::istream &operator>>(std::istream &in, Bignum &t)
@@ -49,9 +64,9 @@ std::istream &operator>>(std::istream &in, Bignum &t)
     return in;
 }
 
-std::ostream &operator<<(std::ostream &out, const Bignum &t)
+std::ostream &operator<<(std::ostream &out, Bignum &t)
 {
-    //注意逆序输出
+    // 注意逆序输出
     for (int i = t.num.size() - 1; i >= 0; i--)
     {
         out << t.num[i];
@@ -59,49 +74,116 @@ std::ostream &operator<<(std::ostream &out, const Bignum &t)
     return out;
 }
 
-Bignum Bignum::operator+ (Bignum &t)
+Bignum Bignum::operator+(Bignum &t)
 {
-    //取最大位数
+    // 取最大位数
     int MAX = this->num.size() > t.num.size() ? this->num.size() : t.num.size();
 
-    //用0补位
-    if(MAX == this->num.size())
+    // 用0补位
+    if (MAX == this->num.size())
     {
-        for(int i = 1; i <= MAX-t.num.size(); i++)
+        for (int i = 1; i <= MAX - t.num.size(); i++)
         {
             t.num.push_back(0);
         }
-    }else
+    }
+    else
     {
-        for(int i = 1; i <= MAX-this->num.size(); i++)
+        for (int i = 1; i <= MAX - this->num.size(); i++)
         {
             this->num.push_back(0);
         }
     }
 
-    //构造返回函数
+    // 构造返回函数
     Bignum a;
-    for(int i = 0; i < MAX; i++)
+    for (int i = 0; i < MAX; i++)
     {
         a.num.push_back(this->num[i] + t.num[i]);
     }
 
-    //加法计算
-    for(int i = 0; i < MAX; i++)
+    // 加法计算
+    for (int i = 0; i < MAX; i++)
     {
-        if(a.num[i] >= 10)
+        if (a.num[i] >= 10)
         {
-            //判断最高位是否需要进位
-            if(i == MAX-1)
+            // 判断最高位是否需要进位
+            if (i == MAX - 1)
             {
                 a.num.push_back(a.num[i] / 10);
                 a.num[i] = a.num[i] % 10;
-            }else
+            }
+            else
             {
-                a.num[i+1] += a.num[i] / 10;
+                a.num[i + 1] += a.num[i] / 10;
                 a.num[i] = a.num[i] % 10;
             }
         }
     }
     return a;
 }
+
+Bignum Bignum::operator+(const long long t)
+{
+    Bignum a;
+    a = std::to_string(t);
+    return (a + *this);
+}
+
+Bignum Bignum::operator*(const long long t)
+{
+    Bignum a;
+    for (int i = 0; i < this->num.size(); i++)
+    {
+        a.num.push_back(t * this->num[i]);
+    }
+
+    // 动态进位
+    for (int i = 0; i < a.num.size(); i++)
+    {
+        if (a.num[i] / 10 > 0)
+        {
+            if (i == a.num.size() - 1)
+            {
+                a.num.push_back(a.num[i] / 10);
+                a.num[i] = a.num[i] % 10;
+            }
+            else
+            {
+                a.num[i + 1] += a.num[i] / 10;
+                a.num[i] = a.num[i] % 10;
+            }
+        }
+    }
+
+    return a;
+}
+
+Bignum Bignum::operator*(const Bignum &t)
+{
+    Bignum a;
+    for (int i = 0; i < this->num.size() + t.num.size(); i++)
+    {
+        a.num.push_back(0);
+    }
+
+    for (int i = 0; i < this->num.size(); i++)
+    {
+        for(int j = 0; j < t.num.size(); j++)
+        {
+            a.num[i+j] += this->num[i] + t.num[j];
+            a.num[i+j+1] += a.num[i+j]/10;
+            a.num[i+j] %= 10;
+        }
+    }
+
+    // 除去前面的0
+    while (a.num.size() > 0 && a.num[a.num.size() - 1] == 0)
+    {
+        a.num.pop_back();
+    }
+
+    return a;
+}
+
+#endif
--- Arbitrary-Precision/test.cpp
+++ Arbitrary-Precision/test.cpp
@@ -1,11 +1,15 @@
-#include"Bignum.h"
+#include "Bignum.h"
 
 int main()
 {
-    Bignum a,b;
-    std::cin >> a;
-    b = a;
-    std::cout << b;
+    Bignum a, b, c;
+    std::cin >> a ;
+    std::cin >> b;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    // c="0100";
+    c = a*b;
+    std::cout << c << std::endl;
 
     std::cin.get();
     std::cin.get();
